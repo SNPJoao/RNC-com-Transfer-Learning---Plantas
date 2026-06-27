1b12f35c967cd9cd5386b166a1919e806f02a0b1
João Vitor Moreira da Silva <jvms1468@gmail.com>
2026-06-27 14:22:52 -0300

    O código utiliza os modelos MobileNetV2 e DenseNet121 para o transfer learning e faz a comparação com learning rates diferentes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-#RNCs com transfer learning em um Dataset de plantas
+RNCs com transfer learning em um Dataset de plantas
+
+O código utiliza 2 modelos diferentes para realizar o transfer learning: O MobileNet_V2 e o DenseNet121.
+Foi utilizado o pytorch para a utilização das IAs e seu treinamento.
+
+O dataset pode ser encontrado no site https://www.kaggle.com/code/atharvaingle/plant-disease-classification-resnet-99-2
--- duasRNCs.py
+++ duasRNCs.py
@@ -29,79 +29,78 @@ NUM_CLASSES = len(dados_treino.classes) # Deve resultar em 38 para o dataset de 
 loader_treino = torch.utils.data.DataLoader(dados_treino, batch_size=BATCH_SIZE, shuffle=True)
 loader_validacao = torch.utils.data.DataLoader(dados_validacao, batch_size=BATCH_SIZE, shuffle=False)
 
-# ==========================================
-# PARTE 2: MONTANDO O MODELO (TRANSFER LEARNING)
-# ==========================================
-# Configurando o hardware (Usa GPU se disponível, senão usa CPU)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Treinando no dispositivo: {device}")
 
-# 2.1 Importando a ResNet50 já treinada
-modelo = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-
-# 2.2 Congelando a base (Evita destruir o conhecimento prévio)
-for param in modelo.parameters():
-    param.requires_grad = False
-
-# 2.3 Trocando a "cabeça" da rede
-# A última camada da ResNet50 original no PyTorch chama-se "fc" (Fully Connected).
-# Pegamos o número de neurônios que entram nela e criamos uma nova saída com 38 neurônios.
-num_features = modelo.fc.in_features
-modelo.fc = nn.Linear(num_features, NUM_CLASSES)
-
-# Envia o modelo para o processador ou placa de vídeo
-modelo = modelo.to(device)
-
 # ==========================================
-# PARTE 3: COMPILADOR E LOOP DE TREINAMENTO
+# PARTE 2: FUNÇÃO PARA TREINAR OS MODELOS 
 # ==========================================
-criterio_erro = nn.CrossEntropyLoss() # A função matemática de perda
-otimizador = optim.Adam(modelo.fc.parameters(), lr=0.001) # Otimiza APENAS a nova camada final
 
-EPOCAS = 2
+def treinar_modelo(modelo, nome_modelo, learning_rate, epocas):
+    print(f"\n---Treinando modelo {nome_modelo} com learning rate {learning_rate}")
+    modelo = modelo.to(device)
+    criterio_erro = nn.CrossEntropyLoss() # A função matemática de perda
+    parametros_treinaveis = filter(lambda p: p.requires_grad, modelo.parameters())
+    otimizador = optim.Adam(parametros_treinaveis, lr=learning_rate) # Otimiza APENAS a nova camada final
 
-print("Iniciando o Treinamento...")
+    melhor_acuracia = 0.0
 
-for epoca in range(EPOCAS):
-    modelo.train() # Avisa ao PyTorch que estamos no modo de treino
-    erro_treino_acumulado = 0.0
-    acertos_treino = 0
-    total_treino = 0
+    for epoca in range(epocas):
+        modelo.train() # Avisa ao PyTorch que estamos no modo de treino
     
-    # --- LOOP DE TREINO ---
-    for imagens, rotulos in loader_treino:
-        imagens, rotulos = imagens.to(device), rotulos.to(device)
-        
-        otimizador.zero_grad()      # 1. Zera a memória de erros do passo anterior
-        saidas = modelo(imagens)    # 2. A rede tenta adivinhar a doença (Forward Pass)
-        erro = criterio_erro(saidas, rotulos) # 3. Calcula o quão errada ela foi
-        erro.backward()             # 4. Calcula a correção necessária (Backpropagation)
-        otimizador.step()           # 5. Aplica a correção nos pesos da última camada
-        
-        # Guardando métricas
-        erro_treino_acumulado += erro.item()
-        _, previsoes = torch.max(saidas, 1)
-        acertos_treino += (previsoes == rotulos).sum().item()
-        total_treino += rotulos.size(0)
-        
-    acuracia_treino = acertos_treino / total_treino
-    
-    # --- LOOP DE VALIDAÇÃO ---
-    modelo.eval() # Avisa ao PyTorch que estamos apenas testando (desliga Dropouts, etc)
-    acertos_val = 0
-    total_val = 0
-    
-    with torch.no_grad(): # Desliga o cálculo de gradientes para economizar memória e ficar rápido
-        for imagens, rotulos in loader_validacao:
+        # --- LOOP DE TREINO ---
+        for imagens, rotulos in loader_treino:
             imagens, rotulos = imagens.to(device), rotulos.to(device)
-            saidas = modelo(imagens)
-            
-            _, previsoes = torch.max(saidas, 1)
-            acertos_val += (previsoes == rotulos).sum().item()
-            total_val += rotulos.size(0)
-            
-    acuracia_val = acertos_val / total_val
+        
+            otimizador.zero_grad()      # 1. Zera a memória de erros do passo anterior
+            saidas = modelo(imagens)    # 2. A rede tenta adivinhar a doença (Forward Pass)
+            erro = criterio_erro(saidas, rotulos) # 3. Calcula o quão errada ela foi
+            erro.backward()             # 4. Calcula a correção necessária (Backpropagation)
+            otimizador.step()           # 5. Aplica a correção nos pesos da última camada
+        
+        # --- LOOP DE VALIDAÇÃO ---
+        modelo.eval() # Avisa ao PyTorch que estamos apenas testando (desliga Dropouts, etc)
+        acertos_val = 0
+        total_val = 0
     
-    print(f"Época [{epoca+1}/{EPOCAS}] | Acurácia Treino: {acuracia_treino:.4f} | Acurácia Validação: {acuracia_val:.4f}")
+        with torch.no_grad(): # Desliga o cálculo de gradientes para economizar memória e ficar rápido
+            for imagens, rotulos in loader_validacao:
+                imagens, rotulos = imagens.to(device), rotulos.to(device)
+                saidas = modelo(imagens)
+            
+                _, previsoes = torch.max(saidas, 1)
+                acertos_val += (previsoes == rotulos).sum().item()
+                total_val += rotulos.size(0)
+            
+        acuracia_val = acertos_val / total_val
+        melhor_acuracia = max(melhor_acuracia, acuracia_val) 
+        print(f"Época [{epoca+1}/{epocas}] | Acurácia Validação: {acuracia_val:.4f}")
 
-print("Treinamento finalizado!")
+    print("Treinamento finalizado!")
+    return melhor_acuracia
+
+# ===================================================
+# PARTE 3: MODELOS PARA TREINAR E COMPARAR RESULTADOS 
+# ===================================================
+
+resultados = {}
+
+densenet = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT)
+for params in densenet.parameters():
+    params.requires_grad = False
+num_features = densenet.classifier.in_features
+densenet.classifier = nn.Linear(num_features, NUM_CLASSES)
+
+mobilenetv2 = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.DEFAULT)
+for param in mobilenetv2.parameters():
+    param.requires_grad = False
+num_features = mobilenetv2.classifier[1].in_features
+mobilenetv2.classifier[1] = nn.Linear(num_features, NUM_CLASSES)
+
+EPOCAS = 5
+
+resultados["DesneNet121 (Lr = 0.001)"] = treinar_modelo(densenet, "DenseNet121", 0.001, EPOCAS)
+resultados["MobileNetV2 (Lr = 0.0005)"] = treinar_modelo(mobilenetv2, "MobileNetV2", 0.0005, EPOCAS)
+
+for nome, acuracia in resultados:
+    print(f"Modelo {nome} | Melhor acurácia = {acuracia:.4f}")
